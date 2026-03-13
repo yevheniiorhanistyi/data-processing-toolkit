386075a7e9d8434d7118fdab5de3c51e9619f73b
Yevhenii Orhanistyi <organistiy@gmail.com>
2026-03-13 11:35:47 +0100

    refactor: use shared calculateFileHash utility

--- src/commands/hash.js
+++ src/commands/hash.js
@@ -1,11 +1,9 @@
-import { createReadStream } from "node:fs";
 import { access, writeFile } from "node:fs/promises";
-import { pipeline } from "node:stream/promises";
-import * as crypto from "node:crypto";
 
 import { parseArgs } from "../utils/argParser.js";
 import { resolvePath } from "../utils/pathResolver.js";
 import { isValidAlgorithm } from "../utils/validators.js";
+import { calculateFileHash } from "../utils/hashHelper.js";
 
 export const hash = async (currentDir, args) => {
   const { input, algorithm = "sha256", save } = parseArgs(args);
@@ -23,10 +21,7 @@ export const hash = async (currentDir, args) => {
 
   try {
     await access(inputFile);
-    const hashGenerator = crypto.createHash(algorithm);
-
-    await pipeline(createReadStream(inputFile), hashGenerator);
-    const finalHash = hashGenerator.digest("hex");
+    const finalHash = await calculateFileHash(inputFile, algorithm);
 
     console.log(`${algorithm}: ${finalHash}`);
 
